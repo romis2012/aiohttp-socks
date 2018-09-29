57255b2090d4d1fca500b07044795ecc90ed1264
rsng <rsng@mail.ru>
2018-09-29 17:02:09 +0300

    Update version

--- aiohttp_socks/__init__.py
+++ aiohttp_socks/__init__.py
@@ -8,7 +8,7 @@ from .errors import (
     SocksConnectionError
 )
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 __all__ = ('SocksConnector', 'SocksVer',
            'SocksError', 'NoAcceptableAuthMethods', 'UnknownAuthMethod',
