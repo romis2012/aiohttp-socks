f3a7e93792c15dd4f5c0942cdd7572b0e5aab331
Roman Snegirev <rsng@mail.ru>
2020-11-25 18:55:55 +0300

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,6 @@ import pytest  # noqa
 
 # noinspection PyProtectedMember
 from python_socks._resolver_async_aio import Resolver as AsyncioResolver
-# noinspection PyProtectedMember
-from python_socks._resolver_async_curio import Resolver as CurioResolver
-# noinspection PyProtectedMember
-from python_socks._resolver_async_trio import Resolver as TrioResolver
-# noinspection PyProtectedMember
-from python_socks._resolver_sync import SyncResolver
 from tests.config import (
     PROXY_HOST_IPV4, PROXY_HOST_IPV6,
     SOCKS5_PROXY_PORT, LOGIN, PASSWORD, SKIP_IPV6_TESTS,
@@ -19,37 +13,17 @@ from tests.config import (
     TEST_HOST_IPV6,
 )
 from tests.http_server import HttpServer, HttpServerConfig
-from tests.mocks import sync_resolve_factory, async_resolve_factory
+from tests.mocks import async_resolve_factory
 from tests.proxy_server import ProxyConfig, ProxyServer
 
 
 @pytest.fixture(scope='session', autouse=True)
 def patch_resolvers():
-    p1 = mock.patch.object(
-        SyncResolver,
-        attribute='resolve',
-        new=sync_resolve_factory(SyncResolver)
-    )
-
-    p2 = mock.patch.object(
-        AsyncioResolver,
-        attribute='resolve',
-        new=async_resolve_factory(AsyncioResolver)
-    )
-
-    p3 = mock.patch.object(
-        TrioResolver,
-        attribute='resolve',
-        new=async_resolve_factory(TrioResolver)
-    )
-
-    p4 = mock.patch.object(
-        CurioResolver,
-        attribute='resolve',
-        new=async_resolve_factory(CurioResolver)
-    )
-
-    with p1, p2, p3, p4:
+    with mock.patch.object(
+            AsyncioResolver,
+            attribute='resolve',
+            new=async_resolve_factory(AsyncioResolver)
+    ):
         yield None
 
 
