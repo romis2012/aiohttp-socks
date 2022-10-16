6d1236e74c6a9d9cda6258c317ff680c46f641b5
Roman Snegirev <rsng@mail.ru>
2022-10-16 10:00:00 +0300

    Mark open_connection/create_connection functions as deprecated

--- aiohttp_socks/utils.py
+++ aiohttp_socks/utils.py
@@ -1,14 +1,31 @@
 import asyncio
+import warnings
 
 from python_socks import ProxyType, parse_proxy_url
 from python_socks.async_.asyncio import Proxy
 
 
-async def open_connection(proxy_url=None, host=None, port=None, *,
-                          proxy_type=ProxyType.SOCKS5,
-                          proxy_host='127.0.0.1', proxy_port=1080,
-                          username=None, password=None, rdns=True,
-                          loop=None, **kwargs):
+async def open_connection(
+    proxy_url=None,
+    host=None,
+    port=None,
+    *,
+    proxy_type=ProxyType.SOCKS5,
+    proxy_host='127.0.0.1',
+    proxy_port=1080,
+    username=None,
+    password=None,
+    rdns=True,
+    loop=None,
+    **kwargs,
+):
+    warnings.warn(
+        'open_connection is deprecated. '
+        'Use https://github.com/romis2012/python-socks directly instead.',
+        DeprecationWarning,
+        stacklevel=2,
+    )
+
     if host is None or port is None:
         raise ValueError('host and port must be specified')  # pragma: no cover
 
@@ -16,8 +33,7 @@ async def open_connection(proxy_url=None, host=None, port=None, *,
         loop = asyncio.get_event_loop()
 
     if proxy_url is not None:
-        proxy_type, proxy_host, proxy_port, username, password \
-            = parse_proxy_url(proxy_url)
+        proxy_type, proxy_host, proxy_port, username, password = parse_proxy_url(proxy_url)
 
     proxy = Proxy.create(
         proxy_type=proxy_type,
@@ -26,40 +42,48 @@ async def open_connection(proxy_url=None, host=None, port=None, *,
         username=username,
         password=password,
         rdns=rdns,
-        loop=loop
+        loop=loop,
     )
 
     sock = await proxy.connect(host, port)
 
     # noinspection PyTypeChecker
-    return await asyncio.open_connection(
-        host=None,
-        port=None,
-        sock=sock,
-        **kwargs
+    return await asyncio.open_connection(host=None, port=None, sock=sock, **kwargs)
+
+
+async def create_connection(
+    proxy_url=None,
+    protocol_factory=None,
+    host=None,
+    port=None,
+    *,
+    proxy_type=ProxyType.SOCKS5,
+    proxy_host='127.0.0.1',
+    proxy_port=1080,
+    username=None,
+    password=None,
+    rdns=True,
+    loop=None,
+    **kwargs,
+):
+    warnings.warn(
+        'create_connection is deprecated. '
+        'Use https://github.com/romis2012/python-socks directly instead.',
+        DeprecationWarning,
+        stacklevel=2,
     )
 
-
-async def create_connection(proxy_url=None, protocol_factory=None,
-                            host=None, port=None, *,
-                            proxy_type=ProxyType.SOCKS5,
-                            proxy_host='127.0.0.1', proxy_port=1080,
-                            username=None, password=None, rdns=True,
-                            loop=None, **kwargs):
     if protocol_factory is None:
-        raise ValueError('protocol_factory '
-                         'must be specified')  # pragma: no cover
+        raise ValueError('protocol_factory must be specified')  # pragma: no cover
 
     if host is None or port is None:
-        raise ValueError('host and port '
-                         'must be specified')  # pragma: no cover
+        raise ValueError('host and port must be specified')  # pragma: no cover
 
     if loop is None:
         loop = asyncio.get_event_loop()
 
     if proxy_url is not None:
-        proxy_type, proxy_host, proxy_port, username, password \
-            = parse_proxy_url(proxy_url)
+        proxy_type, proxy_host, proxy_port, username, password = parse_proxy_url(proxy_url)
 
     proxy = Proxy.create(
         proxy_type=proxy_type,
@@ -68,15 +92,11 @@ async def create_connection(proxy_url=None, protocol_factory=None,
         username=username,
         password=password,
         rdns=rdns,
-        loop=loop
+        loop=loop,
     )
 
     sock = await proxy.connect(host, port)
 
     return await loop.create_connection(
-        protocol_factory=protocol_factory,
-        host=None,
-        port=None,
-        sock=sock,
-        **kwargs
+        protocol_factory=protocol_factory, host=None, port=None, sock=sock, **kwargs
     )
