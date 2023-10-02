6f401841f4dcd7d0f83643f512f98618e71ca700
Roman Snegirev <rsng@mail.ru>
2023-10-02 11:51:18 +0300

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,36 @@
 #!/usr/bin/env python
-import codecs
 import os
 import re
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-version = None
-
-with codecs.open(
-    os.path.join(os.path.abspath(os.path.dirname(__file__)), 'aiohttp_socks', '__init__.py'),
-    'r',
-    'latin1',
-) as fp:
-    try:
-        version = re.findall(r"^__version__ = '([^']+)'\r?$", fp.read(), re.M)[0]
-    except IndexError:
-        raise RuntimeError('Unable to determine version.')
+from setuptools import setup
 
 if sys.version_info < (3, 6, 0):
     raise RuntimeError('aiohttp-socks requires Python 3.6+')
 
-with open('README.md') as f:
-    long_description = f.read()
+
+def get_version():
+    here = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(here, 'aiohttp_socks', '__init__.py')
+    contents = open(filename).read()
+    pattern = r"^__version__ = '(.*?)'$"
+    return re.search(pattern, contents, re.MULTILINE).group(1)
+
+
+def get_long_description():
+    with open('README.md', mode='r', encoding='utf8') as f:
+        return f.read()
+
 
 setup(
     name='aiohttp_socks',
     author='Roman Snegirev',
     author_email='snegiryev@gmail.com',
-    version=version,
+    version=get_version(),
     license='Apache 2',
     url='https://github.com/romis2012/aiohttp-socks',
     description='Proxy connector for aiohttp',
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type='text/markdown',
     packages=['aiohttp_socks'],
     keywords='asyncio aiohttp socks socks5 socks4 http proxy',
